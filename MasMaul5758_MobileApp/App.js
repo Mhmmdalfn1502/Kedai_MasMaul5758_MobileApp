df9bdcb7d05bd08ff1527b40b32e3d270da469cd
zackyyy27 <113814107+zackyyy27@users.noreply.github.com>
2023-07-04 07:51:25 +0700

    Add files via upload

--- MasMaul5758_MobileApp/App.js
+++ MasMaul5758_MobileApp/App.js
@@ -1,19 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import HomePage from "./component/HomePage";
-import LoginPage from "./component/LoginPage";
-import FoodPage from "./component/FoodPage";
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Foodpage from './components/Foodpage';
+import Payment from './components/Payment';
+import Paymentsukses from './components/Paymentsukses';
+import Paymentcash from './components/Paymentcash';
+import PaymentGagal from './components/PaymentGagal';
+import Paymentqr from './components/Paymentqr';
+import AppNavigator from './components/stack/AppNavigator';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <HomePage />
+    <SafeAreaView style={{flex:1}}>
+        {/* <Foodpage/> */}
+        {/* <Payment/> */}
+        {/* <Paymentsukses/> */}
+        {/* <Paymentcash/> */}
+        {/* <PaymentGagal/> */}
+        {/* <Paymentqr/> */}
+        <AppNavigator/>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
